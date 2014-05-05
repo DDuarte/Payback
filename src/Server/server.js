642b6329b0c02f0a662fccd1757b1faf7368e676
Ruben_Cordeiro <ruben.cordeiro.eng@gmail.com>
2014-05-05 16:57:23 +0100

    Replace restify with Express.

--- src/Server/server.js
+++ src/Server/server.js
@@ -1,35 +1,51 @@
+// server.js
+
+// set up ======================================================================
+// get all the tools we need
 //var crypto = require('crypto-js');
 var restify = require("restify");
+
+var express = require('express');
+var compress = require('compression');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy;
+
 var orm = require("orm");
 var database = require("./database");
 
-var server = restify.createServer({ name: "payback" });
-server.use(orm.express("pg://abhihnahgxvxim:WTaDQYg7roQaOx0ieKNDoKZ-V-@ec2-54-197-238-242.compute-1.amazonaws.com:5432/d4ielacnr2v55l?ssl=true&pool=true", {
-    define: database
-}));
 
-// handle the Accept request header
-server.use(restify.acceptParser(server.acceptable));
-
-// handle GET queries, data available in res.query
-server.use(restify.queryParser({ mapParams: false }));
+// create and configure server =================================================
+var server = express();
 
 // if possible compress with gzip
-server.use(restify.gzipResponse());
+server.use(compress());
+
+// read cookies (needed for auth)
+server.use(cookieParser());
 
 // parse the HTTP entity body, data available in res.body
-server.use(restify.bodyParser({
+server.use(bodyParser({
     maxBodySize: 65535,
     mapParams: false
 }));
 
+// setup passport
+server.use(passport.initialize());
+
+server.use(orm.express("pg://abhihnahgxvxim:WTaDQYg7roQaOx0ieKNDoKZ-V-@ec2-54-197-238-242.compute-1.amazonaws.com:5432/d4ielacnr2v55l?ssl=true&pool=true", {
+    define: database
+}));
+
+// define and implement server routes ==========================================
 // TODO: send 403 when not logged in or not current user
 // TODO: implement checksum ( crypto.HmacSHA1( message , encryptionKey ).toString() )
 
 // GET /
 server.get("/", function (req, res, next) {
     res.send(204);
-    return next();
 });
 
 // GET /users/{id}
@@ -39,38 +55,38 @@ server.get("/users/:id", function (req, res, next) {
 
         if (err || !user) {
             res.json(404, { "error": "User " + req.params.id + " not found" });
-        } else {
-            res.json(200, {
-               id: user.id,
-               email: user.email
-            });
+            return;
         }
 
-        return next();
+        res.json(200, {
+            id: user.id,
+            email: user.email
+        });
     });
+
 });
 
 // PATCH /users/{id}
 server.patch("/users/:id", function (req, res, next) {
 
     if (req.body === undefined) {
-        return next(new restify.InvalidContentError("No body defined."));
+        return next("No body defined.");
     }
 
     if (req.body.email === undefined) {
-        return next(new restify.MissingParameterError("Can only change 'email' attribute of the user."));
+        return next("Can only change 'email' attribute of the user.");
     }
 
     req.models.user.get(req.params.id, function (err, user) {
         if (err || !user) {
             res.json(404, { "error": "User " + req.params.id + " not found" });
-            return next();
+            return;
         }
 
-        user.save({ email: req.body.email }, function(err) {
+        user.save({ email: req.body.email }, function (err) {
             if (err || !user) {
                 res.json(403, err);
-                return next();
+                return;
             }
 
             res.json(200, {
@@ -79,6 +95,7 @@ server.patch("/users/:id", function (req, res, next) {
             });
         });
     });
+
 });
 
 // DELETE /users/{id}
@@ -86,20 +103,20 @@ server.del("/users/:id", function (req, res, next) {
 
     req.models.user.get(req.params.id, function (err, user) {
 
-        if (err || !user)
+        if (err || !user) {
             res.json(404, { "error": "User " + req.param.id + " does not exist" });
-        else {
-
-            user.remove(function (err) {
-                if (err) {
-                    res.json(500, err);
-                    return next();
-                }
-
-                res.json(204);
-                return next();
-            });
+            return;
         }
+
+        user.remove(function (err) {
+
+            if (err) {
+                res.json(500, err);
+                return;
+            }
+
+            res.json(204);
+        });
     });
 
 });
@@ -111,14 +128,14 @@ server.get("/users/:id/debts/:debtId", function (req, res, next) {
 
         if (err || !user) {
             res.json(404, {error: "User '" + req.params.id + "' does not exist" });
-            return next();
+            return;
         }
 
         user.getDebts().find({ id: req.params.debtId }, function (err, debts) {
 
             if (err || debts.length === 0) {
                 res.json(404, { error: "Debt '" + req.params.debtId + "' does not exist" });
-                return next();
+                return;
             }
 
             var debt = debts[0];
@@ -129,8 +146,6 @@ server.get("/users/:id/debts/:debtId", function (req, res, next) {
                 date: debt.date,
                 resolved: debt.resolved
             });
-
-            return next();
         });
     });
 
@@ -140,25 +155,25 @@ server.get("/users/:id/debts/:debtId", function (req, res, next) {
 server.patch("/users/:id/debts/:debtId", function (req, res, next) {
 
     if (req.body === undefined) {
-        return next(new restify.InvalidContentError("No body defined."));
+        return next("No body defined.");
     }
 
     if (req.body.value === undefined && req.body.resolved === undefined) {
-        return next(new restify.MissingParameterError("Can only change 'value' or 'resolved' attributes of debts."));
+        return next("Can only change 'value' or 'resolved' attributes of debts.");
     }
 
     req.models.user.exists({ id: req.params.id }, function (err, exists) {
 
         if (err || !exists) {
             res.json(404, { error: "User '" + req.params.id + "' does not exist" });
-            return next();
+            return;
         }
 
         req.models.debt.get(req.params.debtId, function (err, Debt) { // get the debt instance
 
             if (err) {
                 res.json(404, {error: "User '" + req.params.debtId + "' does not exist"});
-                return next();
+                return;
             }
 
             if (req.body.value)
@@ -169,18 +184,16 @@ server.patch("/users/:id/debts/:debtId", function (req, res, next) {
 
             Debt.save(function (err) { // update the debt instance
 
-                if (err)
+                if (err) {
                     res.json(500, err);
-                else
-                    res.json(200, {
-                        debtId: Debt.id,
-                        user: Debt.creditor_id,
-                        value: Debt.value,
-                        date: Debt.date,
-                        resolved: Debt.resolved
-                    });
-
-                return next();
+                }
+                res.json(200, {
+                    debtId: Debt.id,
+                    user: Debt.creditor_id,
+                    value: Debt.value,
+                    date: Debt.date,
+                    resolved: Debt.resolved
+                });
             });
         });
     });
@@ -193,19 +206,18 @@ server.del("/users/:id/debts/:debtId", function (req, res, next) {
     req.models.user.exists({ id: req.params.id }, function (err, exists) {
 
         if (err || !exists) {
-            res.json(404, { error: "User '" + req.params.id +"' does not exist"});
-            return next();
+            res.json(404, { error: "User '" + req.params.id + "' does not exist"});
+            return;
         }
 
         req.models.debt.remove({ id: req.params.debtId }, function (err) {
 
             if (err) {
-                res.json(404, { error: "Debt '" + req.params.debtId +"' does not exist" });
-                return next();
+                res.json(404, { error: "Debt '" + req.params.debtId + "' does not exist" });
+                return;
             }
 
             res.send(204);
-            return next();
         });
     });
 
@@ -218,14 +230,14 @@ server.get("/users/:id/debts", function (req, res, next) {
 
         if (err || !user) {
             res.json(404, { error: "User '" + req.params.id + "' does not exist" });
-            return next();
+            return;
         }
 
         user.getDebts(function (err, debts) {
 
             if (err || !debts) {
                 res.json(500, err);
-                return next();
+                return;
             }
 
             var debts = debts.map(function (debt) {
@@ -246,7 +258,6 @@ server.get("/users/:id/debts", function (req, res, next) {
             };
 
             res.json(200, ret);
-            return next();
         });
     });
 
@@ -256,50 +267,50 @@ server.get("/users/:id/debts", function (req, res, next) {
 server.post("/users/:id/debts", function (req, res, next) {
 
     if (req.body === undefined) {
-        return next(new restify.InvalidContentError("No body defined."));
+        return next("No body defined.");
     }
 
     if (req.body.user === undefined) {
-        return next(new restify.MissingParameterError("Attribute 'user' is missing."));
+        return next("Attribute 'user' is missing.");
     }
 
     if (req.body.value === undefined) {
-        return next(new restify.MissingParameterError("Attribute 'value' is missing."));
+        return next("Attribute 'value' is missing.");
     }
 
     if (isNaN(req.body.value)) {
-        return next(new restify.InvalidArgumentError("Attribute 'value' needs to be a number."));
+        return next("Attribute 'value' needs to be a number.");
     }
 
     var valueStr = req.body.value.toString();
     var splitValueStr = valueStr.split(".");
 
     if (splitValueStr.length > 1 && splitValueStr[1].length > 2) { // more than 2 decimal digits
-        return next(new restify.InvalidArgumentError("Attribute 'value' can't exceed 2 decimal digits."));
+        return next("Attribute 'value' can't exceed 2 decimal digits.");
     }
 
     req.models.user.exists({ id: req.params.id }, function (err, exists) {
 
         if (err) {
             res.json(500, err);
-            return next();
+            return;
         }
 
         if (!exists) {
             res.json(404, {error: "User '" + req.params.id + "' does not exist" });
-            return next();
+            return;
         }
 
         req.models.user.exists({ id: req.body.user }, function (err, exists) {
 
             if (err) {
                 res.json(500, err);
-                return next();
+                return;
             }
 
             if (!exists) {
                 res.json(404, {error: "User '" + req.body.user + "' does not exist" });
-                return next();
+                return;
             }
 
             req.models.debt.create({
@@ -312,7 +323,7 @@ server.post("/users/:id/debts", function (req, res, next) {
 
                 if (err || !debtItem) {
                     res.json(500, err);
-                    return next();
+                    return;
                 }
 
                 res.json(201, {
@@ -323,7 +334,7 @@ server.post("/users/:id/debts", function (req, res, next) {
                     resolved: debtItem.resolved
                 });
 
-                return next();
+                return;
             });
         });
     });
@@ -349,7 +360,6 @@ server.get("/users/:id/balances", function (req, res, next) {
     };
 
     res.json(200, obj);
-    return next();
 });
 
 // GET /users/{id}/friends/{friendId}
@@ -358,18 +368,17 @@ server.get("/users/:id/friends/:friendId", function (req, res, next) {
     req.models.user.get(req.params.id, function (err, user) {
         if (err) {
             res.json(404, { error: "User '" + req.params.id + "' does not exist" });
-            return next();
+            return;
         }
 
         user.getFriends({ id: req.params.friendId }, function (err, friends) {
 
             if (err || friends.length === 0) {
                 res.json(404, { error: "User '" + req.params.friendId + "' is not a friend" });
-                return next();
+                return;
             }
 
             res.json(200, { id: friends[0].id });
-            return next();
         });
     });
 
@@ -382,10 +391,10 @@ server.del("/users/:id/friends/:friendId", function (req, res, next) {
 
         if (err) {
             res.json(500, err);
-            return next();
+            return;
         } else if (!me) {
             res.json(404, { error: "User (me) '" + req.body.id + "' does not exist." });
-            return next();
+            return;
         }
 
         // check if the friend exists
@@ -393,20 +402,19 @@ server.del("/users/:id/friends/:friendId", function (req, res, next) {
 
             if (err) {
                 res.json(500, err);
-                return next();
+                return;
             } else if (!friend) {
                 res.json(404, { error: "User (friend) '" + req.body.id + "' does not exist." });
-                return next();
+                return;
             }
 
             me.removeFriends([friend], function (err) {
                 if (err) {
                     res.json(500, err);
-                    return next();
+                    return;
                 }
 
                 res.send(204);
-                return next();
             });
         });
     });
@@ -419,19 +427,19 @@ server.get("/users/:id/friends", function (req, res, next) {
     req.models.user.get(req.params.id, function (err, me) {
         if (err) {
             res.json(500, err);
-            return next();
+            return;
         } else if (!me) {
             res.json(404, { error: "User (me) '" + req.body.id + "' does not exist." });
-            return next();
+            return;
         }
 
         me.getFriends(function (err, friends) {
             if (err) {
                 res.json(500, err);
-                return next();
+                return;
             } else if (!friends) {
                 res.json(404, { error: "Friends does not exist." });
-                return next();
+                return;
             }
 
             friends = friends.map(function (user) {
@@ -453,21 +461,21 @@ server.get("/users/:id/friends", function (req, res, next) {
 server.post("/users/:id/friends", function (req, res, next) {
 
     if (req.body === undefined) {
-        return next(new restify.InvalidContentError("No body defined."));
+        return next("No body defined.");
     }
 
     if (req.body.id === undefined) {
-        return next(new restify.MissingParameterError("Attribute 'id' is missing."));
+        return next("Attribute 'id' is missing.");
     }
 
     req.models.user.get(req.params.id, function (err, me) {
 
         if (err) {
             res.json(500, err);
-            return next();
+            return;
         } else if (!me) {
             res.json(404, { error: "User (me) '" + req.body.id + "' does not exist." });
-            return next();
+            return;
         }
 
         // check if the friend exists
@@ -475,20 +483,19 @@ server.post("/users/:id/friends", function (req, res, next) {
 
             if (err) {
                 res.json(500, err);
-                return next();
+                return;
             } else if (!friend) {
                 res.json(404, {error: "User (friend) '" + req.body.id + "' does not exist."});
-                return next();
+                return;
             }
 
             me.addFriends([friend], { date: new Date() }, function (err) {
                 if (err) {
                     res.json(500, err);
-                    return next();
+                    return;
                 }
 
                 res.json(201, { id: friend.id });
-                return next();
             });
         });
     });
@@ -501,21 +508,20 @@ server.del("/users/:id/friends", function (req, res, next) {
     req.models.user.get(req.params.id, function (err, me) {
         if (err) {
             res.json(500, err);
-            return next();
+            return;
         } else if (!me) {
             res.json(404, { error: "User (me) '" + req.body.id + "' does not exist." });
-            return next();
+            return;
         }
 
         me.setFriends([], function (err) {
             if (err) {
                 res.json(500, err);
-                return next();
+                return;
             }
         });
 
         res.send(204);
-        return next();
     });
 
 });
@@ -540,7 +546,7 @@ server.get("/users", function (req, res, next) {
 
         if (err) {
             res.json(500, err);
-            return next();
+            return;
         }
 
         users = users.map(function (user) {
@@ -557,8 +563,6 @@ server.get("/users", function (req, res, next) {
 
             res.json(200, filteredUsers);
         }
-
-        return next();
     });
 
 });
@@ -567,23 +571,23 @@ server.get("/users", function (req, res, next) {
 server.post("/users", function (req, res, next) {
 
     if (req.body === undefined) {
-        return next(new restify.InvalidContentError("No body defined."));
+        return next("No body defined");
     }
 
     if (req.body.id === undefined) {
-        return next(new restify.MissingParameterError("Attribute 'id' is missing."));
+        return next("Attribute 'id' is missing.");
     }
 
     if (req.body.email === undefined) {
-        return next(new restify.MissingParameterError("Attribute 'email' is missing."));
+        return next("Attribute 'email' is missing.");
     }
 
     if (!/\S+@\S+\.\S+/.test(req.body.email)) {
-        return next(new restify.InvalidArgumentError("Attribute 'email' is not a valid email address."));
+        return next("Attribute 'email' is not a valid email address.");
     }
 
     if (req.body.passwordHash === undefined) {
-        return next(new restify.InvalidArgumentError("Attribute 'passwordHash' is missing"));
+        return next("Attribute 'passwordHash' is missing");
     }
 
     req.models.user.create({
@@ -595,18 +599,16 @@ server.post("/users", function (req, res, next) {
         if (err) {
 
             if (err.code == 23505) { // unique_violation
-                return next(new restify.InvalidArgumentError("Already exists"));
+                return next("Already exists");
             } else if (err.msg == "invalid-password-length" || err.msg == "invalid-email-format") {
-                return next(new restify.InvalidArgumentError(err.msg));
+                return next(err.msg);
             }
             else {
                 res.json(500, err);
-                return next();
             }
         }
 
         res.json(201, {id: item.id, email: item.email });
-        return next();
     });
 
 });
@@ -614,5 +616,5 @@ server.post("/users", function (req, res, next) {
 var port = process.env.PORT || 1337;
 
 server.listen(port, function () {
-    console.log("%s listening at %s", server.name, server.url);
+    console.log("listening at %s", port);
 });
