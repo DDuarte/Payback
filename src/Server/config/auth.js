788336c3d55139ee692af3885e60ddbe87bedc03
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2014-05-07 06:57:15 +0100

    Add config/auth module.

---
+++ src/Server/config/auth.js
@@ -0,0 +1,11 @@
+// config/auth.js
+
+module.exports = {
+
+    facebookAuth: {
+        clientID: "279961542177568",
+        clientSecret: "72f23d486592a7af12a79c0020457a54",
+        callbackURL: "http://localhost:1337/auth/facebook/callback"
+    }
+};
+
