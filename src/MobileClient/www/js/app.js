659ae15e262927d23dca38b45086d1557b2c207d
Ruben_Cordeiro <ruben.cordeiro.eng@gmail.com>
2014-05-22 12:20:17 +0100

    Add login and signup screens.

--- src/MobileClient/www/js/app.js
+++ src/MobileClient/www/js/app.js
@@ -6,67 +6,79 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('PaybackApp', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-      url: "/app",
-      abstract: true,
-      templateUrl: "templates/menu.html",
-      controller: 'AppCtrl'
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
     })
 
-    .state('app.search', {
-      url: "/search",
-      views: {
-        'app@' :{
-          templateUrl: "templates/search.html"
-        }
-      }
-    })
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
 
-    .state('app.browse', {
-      url: "/browse",
-      views: {
-        'app@' :{
-          templateUrl: "templates/browse.html"
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'app@' :{
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+            .state('login', {
+                url: "/login",
+                templateUrl: "templates/login.html",
+                controller: "LoginCtrl"
+            })
 
-    .state('app.single', {
-      url: "/playlists/:playlistId",
-      views: {
-        'app@' :{
-          templateUrl: "templates/playlist.html",
-          controller: 'PlaylistCtrl'
-        }
-      }
+            .state('signup', {
+                url: "/signup",
+                templateUrl: "templates/signup.html",
+                controller: "SignupCtrl"
+            })
+
+            .state('app', {
+                url: "/app",
+                abstract: true,
+                templateUrl: "templates/menu.html",
+                controller: 'AppCtrl'
+            })
+
+            .state('app.search', {
+                url: "/search",
+                views: {
+                    'app': {
+                        templateUrl: "templates/search.html"
+                    }
+                }
+            })
+
+            .state('app.browse', {
+                url: "/browse",
+                views: {
+                    'app': {
+                        templateUrl: "templates/browse.html"
+                    }
+                }
+            })
+            .state('app.playlists', {
+                url: "/playlists",
+                views: {
+                    'app': {
+                        templateUrl: "templates/playlists.html",
+                        controller: 'PlaylistsCtrl'
+                    }
+                }
+            })
+
+            .state('app.single', {
+                url: "/playlists/:playlistId",
+                views: {
+                    'app': {
+                        templateUrl: "templates/playlist.html",
+                        controller: 'PlaylistCtrl'
+                    }
+                }
+            });
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/login');
     });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
 
--- src/MobileClient/www/js/controllers.js
+++ src/MobileClient/www/js/controllers.js
@@ -1,22 +1,38 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicSideMenuDelegate) {
-    $scope.toggleLeft = function() {
-        $ionicSideMenuDelegate.toggleLeft();
-    }
-})
+    .controller('AppCtrl', function ($scope, $ionicSideMenuDelegate) {
+        $scope.toggleLeft = function () {
+            $ionicSideMenuDelegate.toggleLeft();
+        }
+    })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+    .controller('LoginCtrl', function ($scope, $state) {
+        $scope.login = function () {
+            $state.go('app.search');
+        }
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+        $scope.signin = function() {
+            $state.go('app.search');
+        }
+    })
+
+    .controller('SignupCtrl', function($scope, $state) {
+        $scope.signup = function() {
+            $state.go('app.search');
+        }
+    })
+
+    .controller('PlaylistsCtrl', function ($scope) {
+        $scope.playlists = [
+            { title: 'Reggae', id: 1 },
+            { title: 'Chill', id: 2 },
+            { title: 'Dubstep', id: 3 },
+            { title: 'Indie', id: 4 },
+            { title: 'Rap', id: 5 },
+            { title: 'Cowbell', id: 6 }
+        ];
+    })
+
+    .controller('PlaylistCtrl', function ($scope, $stateParams) {
         //$scope.header_title = "Playlist";
-});
+    });
