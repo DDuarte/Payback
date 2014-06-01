bca8991a2e4321194d84ae125437415cf9c04903
Duarte Duarte <dnpd.dd@gmail.com>
2014-06-01 20:08:07 +0100

    Update platforms/android

--- src/MobileClient/platforms/android/assets/www/cordova_plugins.js
+++ src/MobileClient/platforms/android/assets/www/cordova_plugins.js
@@ -1,17 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/org.apache.cordova.device/www/device.js",
-        "id": "org.apache.cordova.device.device",
-        "clobbers": [
-            "device"
-        ]
-    },
-    {
         "file": "plugins/com.ionic.keyboard/www/keyboard.js",
         "id": "com.ionic.keyboard.keyboard",
         "clobbers": [
             "cordova.plugins.Keyboard"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
         ]
     },
     {
@@ -32,10 +32,10 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.device": "0.2.9",
     "com.ionic.keyboard": "0.0.1",
-    "org.apache.cordova.inappbrowser": "0.4.0",
+    "org.apache.cordova.device": "0.2.9",
     "org.apache.cordova.geolocation": "0.3.7",
+    "org.apache.cordova.inappbrowser": "0.4.0",
     "org.apache.cordova.statusbar": "0.1.3"
 }
 // BOTTOM OF METADATA
